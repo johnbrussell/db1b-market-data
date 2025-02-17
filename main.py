c4734886efd2537d95c36b5bb15b8ea21f10d7fa
JBR <14211416+johnbrussell@users.noreply.github.com>
2025-02-16 21:19:18 -0500

    add input, output data paths

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import sys
+
+
+class DB1B:
+    def __init__(self, output_path, input_paths):
+        assert output_path.endswith('.csv')
+        self._output_path = output_path
+        assert len(input_paths) > 0
+        self._input_paths = input_paths
+
+
 def main():
-    pass
+    db1b = DB1B(sys.argv[1], sys.argv[2:])
 
 
 if __name__ == '__main__':
